9b2ea99ffdb52ec85ca4f09b36c37bd7afb3e6e6
Akashbag2001 <bagakash11@gmail.com>
2025-02-07 00:17:52 +0530

    Second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold flex justify-center items-center w-full h-screen">
+      This will be a webpage with react Three fiber <span className='animate-bounce mx-2 text-red-500'>tommorow</span> <span className='mx-1 animate-spin text-green-600'>:{")"}</span>
+    </h1>
+    </div>
+  )
+}
+
+export default App
